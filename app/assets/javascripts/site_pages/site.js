8488a5ac394d822eda26b94b54516f8a5fb4c8dc
David Ladowitz <david@ladowitz.com>
2015-11-01 14:34:26 -0800

    fixing layout

--- app/assets/javascripts/site_pages/site.js
+++ app/assets/javascripts/site_pages/site.js
@@ -8,12 +8,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require site_pages/commerce-min
-//= require site_pages/common-min
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
 //= require site_pages/ekko-lightbox.min
 //= require site_pages/maplace-0.1.3.min
 //= require site_pages/site.js&helpers.js&sticky
 //= require site_pages/typekit
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
