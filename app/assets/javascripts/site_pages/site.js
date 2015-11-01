2af924b20878d5aa3749e1251c2ae0653f955a1a
David Ladowitz <david@ladowitz.com>
2015-11-01 13:04:41 -0800

    fixing assets for sites

---
+++ app/assets/javascripts/site_pages/site.js
@@ -0,0 +1,19 @@
+// This is a manifest file that'll be compiled into site.js, which will include all the files
+// listed below.
+//
+// Note that 'site_pages/site.js' has been added to Rails.application.config.assets.precompile in assets.rb
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+
+//= require site_pages/commerce-min
+//= require site_pages/common-min
+//= require site_pages/ekko-lightbox.min
+//= require site_pages/maplace-0.1.3.min
+//= require site_pages/site.js&helpers.js&sticky
+//= require site_pages/typekit
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
