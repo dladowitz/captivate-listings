6af8c4045c7945e9091d1a7b0ded128213e8ea3f
David Ladowitz <david@ladowitz.com>
2015-11-04 23:53:21 -0800

    updating mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,26 +2,27 @@ class UserMailer < ApplicationMailer
   # layout "user_mailer"  # css is not showing up properly when using a layout
 
   # TODO need to change this to work in production
-  RESET_LINK_BASE = "http://localhost:3000/reset_password/"
+  # RESET_LINK_BASE = "http://localhost:3000/reset_password/"
 
-  def registration_email(registration)
-    @registration = registration
-    attachments.inline['logo_handwriting.png'] = File.read('app/assets/images/logo_handwriting.png')
-
-    mail(to: @registration.email, subject: "Captivate Listings - Registration")
-  end
+  # Pretty sure we don't have registrations anymore
+  # def registration_email(registration)
+  #   @registration = registration
+  #   attachments.inline['logo_handwriting.png'] = File.read('app/assets/images/logo_handwriting.png')
+  #
+  #   mail(to: @registration.email, subject: "Captivate Listings - Registration")
+  # end
 
   def signup_email(user)
     @user = user
-    attachments.inline['logo_handwriting.png'] = File.read('app/assets/images/logo_handwriting.png')
+    attachments.inline['captivatelogo.png'] = File.read('app/assets/images/captivatelogo.png')
 
     mail(to: @user.email, subject: "Captivate Listings - Signup")
   end
 
   def request_password(user, token)
     @user = user
-    @reset_link = RESET_LINK_BASE + token
-    attachments.inline['logo_handwriting.png'] = File.read('app/assets/images/logo_handwriting.png')
+    @reset_link = root_url + "reset_password/" + token
+    attachments.inline['captivatelogo.png'] = File.read('app/assets/images/captivatelogo.png')
 
     mail(to: @user.email, subject: "Captivate Listings - Password Reset")
   end
