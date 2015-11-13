4a7bfb7554f4aac8f0f0c45ccb7834076f0875a3
David Ladowitz <david@ladowitz.com>
2015-11-12 23:51:28 -0800

    getting form to work

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,6 +1,21 @@
 class LandingPagesController < ApplicationController
+  # This is probably a bad idea. Need to update contact form to haml and use form_tag
+  skip_before_filter :verify_authenticity_token, :only => [:contact_form]
+  layout "guest_pages/guest_layout"
 
   def landing
-    render layout: "guest_pages/guest_layout"
+
   end
+
+  def contact_form
+    @message = {name: params[:name], email: params[:email], body: params[:body]}
+    ContactMailer.contact_us_email(@message).deliver
+
+    render: contact_confirmation
+  end
+
+  def contact_confirmation
+
+  end
+
 end
