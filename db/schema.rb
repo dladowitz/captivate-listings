44ef38a872256a35b5475f695186da6bf16fc4f2
David Ladowitz <david@ladowitz.com>
2015-11-15 20:36:09 -0800

    some mess with custom domains

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116034759) do
+ActiveRecord::Schema.define(version: 20151116042256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 20151116034759) do
     t.integer  "zip"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.string   "domain_type"
-    t.string   "domain"
     t.hstore   "details"
     t.integer  "user_id"
     t.boolean  "enabled",       default: true, null: false
