8ff8d652b41419353797cef1a8a7e0b88e85068f
David Ladowitz <david@ladowitz.com>
2015-11-15 19:47:12 -0800

    adding custom domains

---
+++ app/controllers/custom_domains_controller.rb
@@ -0,0 +1,11 @@
+class CustomDomainsController < ApplicationController
+  def router
+    property = Property.find_by_custom_domain(self.env["REQUEST_PATH"])
+    if propery
+      redirect_to property.site
+    else
+      flash[:danger] = "That custom domain is not registered in our system"
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,9 @@ Rails.application.routes.draw do
 
   resources :password_resets, only: [:create]
 
+  # custom url routing
+  get "/noevalleycondo", to: "custom_domains#router", as: :domain_domains
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
