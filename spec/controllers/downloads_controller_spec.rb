2ae55e56b9a801def34f28ebfdb24280c8656ad8
David Ladowitz <david@ladowitz.com>
2016-02-10 22:30:35 -0800

    adding controller specs for downloads controller

--- spec/controllers/downloads_controller_spec.rb
+++ spec/controllers/downloads_controller_spec.rb
@@ -2,15 +2,42 @@ require 'rails_helper'
 
 describe DownloadsController do
   before :each do
-    @user = create :user
+    @user       = create :user
+    @property   = create :property, user: @user
+    @disclosure = create :disclosure, property: @property
+  end
+
+  describe "POST index" do
+    context "with valid params" do
+      subject { get :index, user_id: @user, property_id: @property }
+
+      it "renders the index template" do
+        subject
+        expect(response).to render_template :index
+      end
+
+      it "finds the correct user" do
+        subject
+        expect(assigns(:user)).to eq @user
+      end
+
+      it "finds the correct property" do
+        subject
+        expect(assigns(:property)).to eq @property
+      end
+
+      it "finds the property downloads" do
+        @user2 = create :user
+        download = @property.downloads.create(user: @user2, disclosure: @disclosure)
+        subject
+        expect(assigns(:downloads)).to include download
+      end
+    end
   end
 
   describe "POST create" do
     context "with valid params" do
-        let(:property)   { create :property }
-        let(:disclosure) { create :disclosure}
-
-        subject { post :create, user_id: @user.id, property_id: property.id, disclosure_id: disclosure.id }
+      subject { post :create, user_id: @user, property_id: @property, disclosure_id: @disclosure }
 
       it "creates a new download in the database" do
         # For some reason 6 photos are created before subject runs. Not sure why.
